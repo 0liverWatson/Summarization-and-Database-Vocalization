fc263d41c33ccacbe9beaf20b8b79bfa5a8fc280
Nishitha Nancy Lima <n.lima@3dqr.de>
2020-12-13 20:42:34 +0100

    Testing with spoken.js file

--- frontend/src/app/summarize-queries/summarize-queries.component.ts
+++ frontend/src/app/summarize-queries/summarize-queries.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import spoken from "../../assets/spoken.js";
 
@@ -8,23 +9,32 @@ import spoken from "../../assets/spoken.js";
 })
 export class SummarizeQueriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   tables: string[];
+  listeningText: string;
 
   ngOnInit(): void {
+    this.getTableData();
 
     this.tables = ['Table1', 'Table2', 'Table3', 'Table4'];
 
+    spoken.say('Hello, Ask Something').then(speech => {
+      spoken.listen().then(transcript => {
+        console.log("Answer: " + transcript);
+        this.listeningText = transcript;
+      })
+    });
+
     var createNewString = function (oldStringObj, string) {
       var _new = new String(string);
-    
+
       var keys = Object.keys(oldStringObj);  // returns only custom properties (not part of prototype)
-      for (var i=0,n=keys.length; i<n; i++){
+      for (var i = 0, n = keys.length; i < n; i++) {
         var key = keys[i];
-    
+
         if (Number.isInteger(+key)) {
-           continue;                         // skip property if it's a numbered key
+          continue;                         // skip property if it's a numbered key
         }
         _new[key] = oldStringObj[key];       // simple assignment (not a deep copy) -- room for improvement
       }
@@ -32,44 +42,41 @@ export class SummarizeQueriesComponent implements OnInit {
     };
 
     // Setup Listener Events
-spoken.listen.on.partial( transcript => { transcript=transcript.toLowerCase(); if (transcript.includes("laura")||transcript.includes("elora")||transcript.includes("alora")||transcript.includes("ellora")||transcript.includes("allure")){ 
-  var arr=transcript.split('laura').join(',').split('elora').join(',').split('ellora').join(',').split('alora').join(',').split('allure').join(',').split(',')
-  transcript=createNewString( transcript, arr[arr.length-1]);
-  console.log("Captured Transcript: "+transcript);} //Here we could send to the chatbot...
-  } );
-  spoken.listen.on.start(   voice      => { } );
-  
-  spoken.listen.on.end(this.continueCapture);
-  spoken.listen.on.error(this.continueCapture);
-  
-  this.startCapture();
+    spoken.listen.on.partial(transcript => {
+      transcript = transcript.toLowerCase(); if (transcript.includes("laura") || transcript.includes("elora") || transcript.includes("alora") || transcript.includes("ellora") || transcript.includes("allure")) {
+        var arr = transcript.split('laura').join(',').split('elora').join(',').split('ellora').join(',').split('alora').join(',').split('allure').join(',').split(',')
+        transcript = createNewString(transcript, arr[arr.length - 1]);
+        console.log("Captured Transcript: " + transcript);
+      } //Here we could send to the chatbot...
+    });
 
-
-   spoken.voices()
-    .filter(  voice => voice.lang.indexOf('en') == 0 )
-    .map(   voice => voice.name ).slice( 7, 8 )
-    .forEach( async voice => console.log(
-        (await spoken.say( 'Hi.', voice )).voice.name
-    ));
+    spoken.listen.on.end(this.continueCapture);
+    spoken.listen.on.error(this.continueCapture);
 
   }
 
-startCapture() { 
-    spoken.listen({ continuous : true }).then( transcript =>
-        console.log('Started Listening')
-    ).catch( e => true );
-}
+  startCapture() {
+    spoken.listen({ continuous: true }).then(transcript =>
+      console.log('Started Listening')
+    ).catch(e => true);
+  }
 
-async continueCapture() {
-  await spoken.delay(1);
-  spoken.listen.stop();
-  console.log('Stopped Listening');
-  if (spoken.recognition.continuous) this.startCapture();
-}
+  async continueCapture() {
+    await spoken.delay(1);
+    spoken.listen.stop();
+    console.log('Stopped Listening');
+    if (spoken.recognition.continuous) this.startCapture();
+  }
 
-stopCapture() {
-  spoken.recognition.continuous = false;
-  spoken.listen.stop();
-}
+  stopCapture() {
+    spoken.recognition.continuous = false;
+    spoken.listen.stop();
+  }
+
+  getTableData() {
+    this.http.get('assets/tables.json').subscribe(data => {
+      this.tables = data['tables'];
+    });
+  }
 
 }
